2a718a295f3e3a8e91ca0eab6515601dfe441c9a
happyrina <riniversal@gmail.com>
2023-03-24 03:03:11 +0000

    new

--- c/gcc/age.c
+++ c/gcc/age.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "myheader.h"
 
+<<<<<<< HEAD
 void main() {
+=======
+void main(){
+>>>>>>> bafb65b (super)
   printf("My age is %d \n", AGE);
 }
--- c/gcc/lib/libmy.h
+++ c/gcc/lib/libmy.h
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 extern int plus(int, int);
 extern int minus(int, int);
+=======
+extern int plus (int, int);
+extern int minus (int, int);
+>>>>>>> bafb65b (super)
