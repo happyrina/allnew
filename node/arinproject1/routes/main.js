c6cd07fce8dc28714501f60e401e4b1b698cf3e5
happyrina <riniversal@gmail.com>
2023-04-28 15:12:27 +0900

    miniproject1

--- node/arinproject1/routes/main.js
+++ node/arinproject1/routes/main.js
@@ -34,7 +34,7 @@ function templete_nodata(res) {
     res.end(templete);
 }
 
-function templete_result(result, res) {
+function templete_result(dataUser, res) {
     res.writeHead(200);
     var templete = `
         <!doctype html>
@@ -46,16 +46,19 @@ function templete_result(result, res) {
         </head>
         <body>
         <table border="1" style="margin;auto;">
-        <thead>
-            <tr><th>User ID</th></tr>
-         </thead>
+       <thead>
+        <tr><th>nickName</th><th>userID</th><th>userPW</th><th>phone</th></tr>
+    </thead>
     <tbody>
     `;
-    for (var i = 0; i < result.length; i++) {
+    for (var i = 0; i < restbl.length; i++) {
         templete += `
     <tr>
-        <td>${result[i]['userid']}</td>
-        <td>${result[i]['passwd']}</td>
+        <td>${dataUser[i]['nickName']}</td>
+        <td>${dataUser[i]['userID']}</td>
+        <td>${restbl[i]['shopId']}</td>
+        <td>${dataUser[i]['userPW']}</td>
+        <td>${dataUser[i]['phone']}</td>
     </tr>
     `;
     }
@@ -69,7 +72,9 @@ function templete_result(result, res) {
 }
 
 
-//login page - user, admin
+
+
+//login page - user, admin / id와 pw가 맞지 않으면 "id와 pw를 확인해 주세요!"라는 문구 띄우기
 app.post('/login', (req, res) => {
     const { userID, userPW } = req.body;
     const query = connection.query("SELECT * FROM dataUser WHERE userID=? AND userPW=?", [userID, userPW]);
@@ -77,34 +82,46 @@ app.post('/login', (req, res) => {
         // console.log(query)
         // res.send({ 'ok': false, 'queryt': query.affectedRows, 'text': '입력을 확인하세요' })
         //res.redirect로 해야 웹 서버에 보여줌
-        res.redirect('/login?error=true');
+        // res.redirect('/');
+        res.send("<script>alert('공백을 확인해주세요'); window.location.replace('/index.html');</script>")
+
     }
     else if (userID == "admin") {
         // res.send({ 'ok': true, 'query': query.affectedRows, 'text': '관리자가 로그인 했습니다!' })
-        console.log(userID + " => 관리자가 로그인했습니다");
-        res.redirect('admin.html');
+        // console.log(userID + " => 관리자가 로그인했습니다");
+        res.send("<script>alert('관리자가 로그인했습니다:)'); window.location.replace('/admin.html');</script>")
+        // res.redirect('admin.html');
     } else {
         // res.send({ 'ok': true, 'query': query.affectedRows, 'text': `사용자 ${userID} 가 로그인 했습니다.` })
-        res.redirect('main.html');
+        res.redirect('main-login.html');
     };
 });
+
+
 
 //register page 
 app.post("/register", (req, res) => {
     const { nickName, userID, userPW, phone } = req.body;
     if (nickName == "" || userID == "" || userPW == "" || phone == "") {
-        res.send({ 'ok': true, 'text': '입력내용을 확인해 주세요' })
+        // res.send({ 'ok': true, 'text': '입력내용을 확인해 주세요' })
+        // res.redirect('/login?error=true');
+        // res.send("<script>alert('입력내용을 확인해 주세요!'); window.location.replace('/register.html');</script>")
+        res.send("<script>alert('입력내용을 확인해 주세요!'); window.location.replace('/register.html');</script>");
     } else {
         let result1 = connection.query("select * from dataUser where nickName=?", [nickName]);
         let result2 = connection.query("select * from dataUser where userID=?", [userID]);
         if (result1.length > 0) {
-            res.send({ 'ok': true, 'result1': result1.affectedRows, 'text': '이미 존재하는 닉네임입니다' })
+            // res.send({ 'ok': true, 'result1': result1.affectedRows, 'text': '이미 존재하는 닉네임입니다' })
+            res.send("<script>alert('이미 존재하는 닉네임입니다'); window.location.replace('/register.html');</script>")
         } else if (result2.length > 0) {
-            res.send({ 'ok': true, 'result2': result2.affectedRows, 'text': '이미 존재하는 아이디입니다.' })
+            // res.send({ 'ok': true, 'result2': result2.affectedRows, 'text': '이미 존재하는 아이디입니다.' })
+            res.send("<script>alert('이미 존재하는 아이디입니다.'); window.location.replace('/register.html');</script>")
+
         } else {
             let result = connection.query("insert into dataUser values (?,?,? ,?)", [nickName, userID, userPW, phone]);
-            console.log(result);
-            res.send({ "ok": true, "result": result.affectedRows, 'text': "회원가입이 완료되었습니다." })
+            // console.log(result);
+            // res.send({ "ok": true, "result": result.affectedRows, 'text': "회원가입이 완료되었습니다." })
+            res.send("<script>alert('회원가입이 완료되었습니다! 다시 로그인을 해주세요!'); window.location.replace('/index.html');</script>")
         }
     }
 });
@@ -113,42 +130,118 @@ app.post("/register", (req, res) => {
 
 //admin page
 //user select
+//user select
 app.get("/user/select", (req, res) => {
     const result = connection.query('select * from dataUser');
     console.log(result);
     res.send({ "ok": true, "result": result });
 });
 
-// 
-
 //user selectQuery
-app.post("/user/selectQuery", (req, res) => {
-    const userID = req.body.userID;
-    console.log(req.body);
-    const result = connection.query("select * from dataUser where userID=?", [userID]);
-    console.log(result);
-    res.send({ "ok": true, "result": result });
-})
+// app.get("/user/selectQuery", (req, res) => {
+//     const userID = req.body.userID;
+//     console.log(req.body);
+//     const result = connection.query("select * from dataUser where userID=?", [userID]);
+//     console.log(result);
+//     res.send({ "ok": true, "result": result });
+// });
 
-//user insert
-app.post("/user/insert", (req, res) => {
-    const { nickName, userID, userPW, phone } = req.body;
-    let result = "";
-    if (nickName == "" || userID == "" || userPW == "" || phone == "") {
-        res.send({ "ok": true, "result": result.affectedRows, 'text': "공백을 채워주세요!" });
-
+app.get('/user/selectQuery', (req, res) => {
+    const userID = req.query.userID;
+    const result = connection.query("SELECT * FROM dataUser where userID=?", [userID]);
+    if (userID == "") {
+        // res.send('원하는 동을 입력하세요.')
+        res.write("<script>alert('ID를 입력하세요')</script>");
     } else {
-        result = connection.query("select * from dataUser where nickName=? OR userID=?", [nickName, userID]);
-        if (result.length > 0) {
-            res.send({ "ok": true, "result": result.affectedRows, 'text': "이미 존재하는 닉네임과 id 입니다." });
+        if (result.length == 0) {
+            res.write("<script>alert('존재하지 않는 사용자입니다.')</script>")
         } else {
-            result = connection.query("insert into dataUser values (?, ?, ?, ?)", [nickName, userID, userPW, phone]);
-            console.log(result);
-            res.send({ "ok": true, "result": result.affectedRows, "text": "새로운 사용자가 추가되었습니다." });
-            //({ ok : true 하고 result를 넣으면 알 수 있음})
+            res.writeHead(200);
+            var template = `
+       <!doctype html>
+       <html>
+       <head>
+           <title>Result</title>
+           <meta charset="utf-8">
+       </head>
+       <body>
+       <table border="1" style="margin:auto; text-align:center;">
+       <thead>
+           <tr><th>nickName</th><th>User ID</th><th>Password</th><th>phone</th></tr>
+       </thead>
+       <tbody>
+       `;
+            for (var i = 0; i < result.length; i++) {
+                template += `
+       <tr>
+       <td>${result[i]['nickName']}</td>
+           <td>${result[i]['userID']}</td>
+           <td>${result[i]['userPW']}</td>
+           <td>${result[i]['phone']}</td>
+       </tr>
+       `;
+            }
+            template += `
+       </tbody>
+       </table>
+       </body>
+       </html>
+   `;
         }
+        res.end(template);
     }
 })
+
+
+//user insert
+// app.post("/user/insert", (req, res) => {
+//     const { nickName, userID, userPW, phone } = req.body;
+//     let result = "";
+//     if (nickName == "" || userID == "" || userPW == "" || phone == "") {
+//         res.send({ "ok": true, "result": result.affectedRows, 'text': "공백을 채워주세요!" });
+
+//     } else {
+//         result = connection.query("select * from dataUser where nickName=? OR userID=?", [nickName, userID]);
+//         if (result.length > 0) {
+//             res.send({ "ok": true, "result": result.affectedRows, 'text': "이미 존재하는 닉네임과 id 입니다." });
+//         } else {
+//             result = connection.query("insert into dataUser values (?, ?, ?, ?)", [nickName, userID, userPW, phone]);
+//             console.log(result);
+//             res.send({ "ok": true, "result": result.affectedRows, "text": "새로운 사용자가 추가되었습니다." });
+//             //({ ok : true 하고 result를 넣으면 알 수 있음})
+//         }
+//     }
+// })
+
+// app.post("/user/insert", async (req, res) => {
+//     const { nickName, userID, userPW, phone } = req.body;
+//     let result = "";
+//     if (nickName == "" || userID == "" || userPW == "" || phone == "") {
+//         res.send({ "ok": true, "result": result.affectedRows, 'text': "공백을 채워주세요!" });
+
+//     } else {
+//         result = await new Promise((resolve, reject) => {
+//             connection.query("select * from dataUser where userID=?", [userID], (err, rows) => {
+//                 if (err) reject(err);
+//                 else resolve(rows);
+//             });
+//         });
+//         if (result.length > 0) {
+//             res.send({ "ok": true, "result": result.affectedRows, 'text': "이미 존재하는 닉네임과 id 입니다." });
+//         } else {
+//             result = await new Promise((resolve, reject) => {
+//                 connection.query("insert into dataUser values (?, ?, ?, ?)", [nickName, userID, userPW, phone], (err, rows) => {
+//                     if (err) reject(err);
+//                     else resolve(rows);
+//                 });
+//             });
+//             console.log(result);
+//             res.send({ "ok": true, "result": result.affectedRows, "text": "새로운 사용자가 추가되었습니다." });
+
+//         }
+//     }
+// })
+
 
 
 //user update
@@ -200,21 +293,5 @@ app.get('/select/userID', (req, res) => {
     res.send({ "ok": true, "result": result });
 })
 
-//감채
-//user screen home,away->kia,doosan select
-app.get('/selectHA', (req, res) => {
-    let result = ""
-    const { Home, Away } = req.query;
-    if (Home == "" && Away != "") {
-        result = connection.query("select * from dataSchedule where away=?", [Away])
-    } else if (Home != "" && Away == "") {
-        result = connection.query("select * from dataSchedule where home =?", [Home])
-    } else {
-        result = connection.query("select * from dataSchedule where home =? and away=?", [Home, Away])
-    }
-    console.log(result)
-
-    res.send(result)
-})
 
 module.exports = app;
